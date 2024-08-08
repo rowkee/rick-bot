d360f1ba4185507c92ce6c512fd04732d1416c87
rowkee <rick.keenan10@gmail.com>
2024-08-08 10:54:54 +0100

    updated title to include cheese

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "🍋 Rick Bot 🤖",
+  title: "🍋 Rick Bot 🧀",
   description: "Use if you miss Rick",
 };
 
